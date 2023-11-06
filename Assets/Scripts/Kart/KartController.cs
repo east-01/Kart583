f6ec325581f2cde6103d2052a693e273554d54bf
east-01 <mullen.ethan@yahoo.com>
2023-11-06 14:57:16 -0800

    sync

--- Assets/Scripts/Kart/KartController.cs
+++ Assets/Scripts/Kart/KartController.cs
@@ -16,8 +16,13 @@ public class KartController : MonoBehaviour
 	// Runtime variables
 	[SerializeField]
     private float rotationInput;
+	
 	[SerializeField]
-	private float velocity;
+	private Vector2 velocity;
+	private float speed { get { return velocity.magnitude; } }
+	private float speedRatio { get { return speed/maxSpeed; } }
+	
+	private Vector2 driftDirection;
 
     private void Start()
     {
@@ -38,33 +43,50 @@ public class KartController : MonoBehaviour
 		switch(state) { 
 			case KartState.DRIVING:	
 				if(throttle > 0) {
-					velocity += throttle * acceleration * Time.deltaTime;
-					if(velocity > maxSpeed) velocity = maxSpeed;
-				} else { 
-					velocity -= passiveDeceleration * Time.deltaTime;
+					velocity = Vector2.ClampMagnitude(transform.forward * (throttle * acceleration + speed) * Time.deltaTime, maxSpeed);
+				} else {
+					// Shorten velocity vector by the passive deceleration rate. TODO: Add brakes
+					velocity = velocity.normalized * (speed - passiveDeceleration*Time.deltaTime);
 					// WARNING: This might cause the kart to stop if it bounced off the wall and rolled backwards
-					if(velocity < 0) velocity = 0; 
+					if(speed < 0) velocity = Vector2.zero; 
 				}
 
-				rotationInput = turn.x * (velocity / maxSpeed);
+				rotationInput = turn.x * speedRatio;
+				break;
+			case KartState.DRIFTING:
+				
+				transform.forward = turn;
+				
+				break;
+			case KartState.REVERSING:
+				break;
+		}
+    
+		Vector3 movement = transform.forward * velocity * maxSpeed * Time.deltaTime;
+        rb.MovePosition(rb.position + movement);
+
+
+		float rotation = rotationInput * rotationSpeed * TurnFunction(0.1f, 0.3f, 0.5f, speedRatio) * Time.deltaTime;
+        Quaternion deltaRotation = Quaternion.Euler(0f, rotation, 0f);
+        rb.MoveRotation(rb.rotation * deltaRotation);			
+		
+	}
+
+	/** The callback from KartStateManager indicating when we've changed state.
+	 *  Thrown immediately before the state changes, newState is the state we're changing to. */
+	public void StateChanged(KartState newState) { 
+		switch(state) { 
+			case KartState.DRIVING:
+				transform.forward = velocity;
 				break;
 			case KartState.DRIFTING:
 				break;
 			case KartState.REVERSING:
 				break;
+			default:
+				break;
 		}
-    }
-
-    private void FixedUpdate()
-    {
-		Vector3 movement = transform.forward * velocity * maxSpeed * Time.fixedDeltaTime;
-        rb.MovePosition(rb.position + movement);
-
-
-		float rotation = rotationInput * rotationSpeed * TurnFunction(0.1f, 0.3f, 0.5f, velocity / maxSpeed) * Time.fixedDeltaTime;
-        Quaternion deltaRotation = Quaternion.Euler(0f, rotation, 0f);
-        rb.MoveRotation(rb.rotation * deltaRotation);
-    }
+	}
 
 	/** Gets turn power percentage from a speed percentage.
 		See desmos: https://www.desmos.com/calculator/tmojijx91i 
