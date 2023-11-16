d21fd0d7373a8f34b4b27e28a984df13dbe52f25
east-01 <mullen.ethan@yahoo.com>
2023-11-16 12:04:14 -0800

    sync

--- Assets/Scripts/World/ItemBoxAnimator.cs
+++ Assets/Scripts/World/ItemBoxAnimator.cs
@@ -8,18 +8,28 @@ public class ItemBoxAnimator : MonoBehaviour
 	public AnimationCurve heightAnimation;
 	public float height = 1f;
 	public float speed = 1f;
+	public float animationLength = 2f;
 
 	private Vector3 initialPosition;
+	private float lifetime;
 
-	private void Awake()
+	private void Start()
 	{
 		initialPosition = transform.position;
+		lifetime = 0;
 	}
 
 	void Update()
     {
-     
-		transform.position =- Vector3.up*heightAnimation.Evaluate();
-		
+		transform.position = initialPosition + Vector3.up*height*heightAnimation.Evaluate(AnimationProgress);
+		lifetime += Time.deltaTime;		
     }
+
+	private void OnTriggerEnter(Collider other)
+	{
+		
+	}
+
+	public float AnimationProgress { get { return (lifetime%animationLength)/animationLength; } }
+
 }
