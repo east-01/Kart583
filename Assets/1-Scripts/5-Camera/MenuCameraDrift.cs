6aea00fe6856740155dc2b7cf1f8e20d0aa09af4
east-01 <mullen.ethan@yahoo.com>
2023-12-16 11:33:17 -0800

    made new title screen

--- Assets/1-Scripts/5-Camera/MenuCameraDrift.cs
+++ Assets/1-Scripts/5-Camera/MenuCameraDrift.cs
@@ -4,17 +4,22 @@ public class MenuCameraDrift : MonoBehaviour
 {
     public float rotationSpeed = 1.0f;
     public float maxRandomOffset = 30.0f;
+    public bool startRandomized = true;
+    
     private Vector3 rotVector;
-    public float rotVectorSelectTime;
+    private float rotVectorSelectTime;
+
+    void Start() 
+    {
+        if(startRandomized) {
+            rotVector = RollNewVector(); 
+        }
+    }
 
     void Update()
     {
         if(rotVectorSelectTime <= 0) {
-            rotVector = new Vector3(
-                Random.Range(-maxRandomOffset, maxRandomOffset),
-                Random.Range(-maxRandomOffset, maxRandomOffset),
-                Random.Range(-maxRandomOffset, maxRandomOffset)
-            );
+            rotVector = RollNewVector();
             rotVectorSelectTime = Random.Range(15, 45);
         } else {
             rotVectorSelectTime -= Time.deltaTime;
@@ -24,4 +29,14 @@ public class MenuCameraDrift : MonoBehaviour
         transform.Rotate(Vector3.up, (rotationSpeed*Random.Range(-maxRandomOffset, maxRandomOffset)) * Time.deltaTime);
         transform.Rotate(rotVector * Time.deltaTime);
     }
+
+    public Vector3 RollNewVector() 
+    {
+        return new(
+            Random.Range(-maxRandomOffset, maxRandomOffset),
+            Random.Range(-maxRandomOffset, maxRandomOffset),
+            Random.Range(-maxRandomOffset, maxRandomOffset)
+        );
+    }
+
 }
