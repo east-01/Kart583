bfdb866b15bc333bc8f0333821fba3163cda4137
east01 <mullen.ethan@yahoo.com>
2024-04-09 14:03:21 -0700

    fixed build script

--- Assets/Editor/BuildScript.cs
+++ Assets/Editor/BuildScript.cs
@@ -3,35 +3,8 @@ using UnityEngine;
 
 public class BuildScript
 {
-    public static void PerformBuild(string target)
+    public static void PerformBuild()
     {
-        BuildTarget buildTarget;
-
-        switch (target)
-        {
-            case "-windows":
-                buildTarget = BuildTarget.StandaloneWindows;
-                break;
-            case "-macos":
-                buildTarget = BuildTarget.StandaloneOSX;
-                break;
-            case "-linux":
-                buildTarget = BuildTarget.StandaloneLinux64;
-                break;
-            case "-android":
-                buildTarget = BuildTarget.Android;
-                break;
-            case "-ios":
-                buildTarget = BuildTarget.iOS;
-                break;
-            case "-webgl":
-                buildTarget = BuildTarget.WebGL;
-                break;
-            default:
-                UnityEngine.Debug.LogError("Invalid build target specified.");
-                return;
-        }
-
-        BuildPipeline.BuildPlayer(EditorBuildSettings.scenes, $"{Application.dataPath}/../Builds", buildTarget, BuildOptions.None);
+        BuildPipeline.BuildPlayer(EditorBuildSettings.scenes, $"{Application.dataPath}/../Builds", BuildTarget.StandaloneWindows, BuildOptions.None);
     }
 }
