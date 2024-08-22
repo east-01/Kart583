97d7bf11c48b7d1c57f7d60a8903f7009ac121bc
bowlerguy66 <mullen.ethan@yahoo.com>
2024-08-21 18:23:13 -0700

    yeah, branch name explains it

---
+++ Assets/0-LocalPackages/EMNetworking/NetworkPlayerDataManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Will appear once connected to the server, connects player data to server
+/// </summary>
+public class NetworkPlayerDataManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
